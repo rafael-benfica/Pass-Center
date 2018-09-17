d8e6eaa96c0baafd5b0b177f89e7d3b3d88c9829
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-09-17 11:39:10 -0300

    Inicio do projeto & Criação do componente APP
    *Foram criados os arquivos base do projeto.
    *Foi criado o componente APP, equivalente a Master Page.

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <nav class="grey lighten-3">  
+        <a href="#" class="ajuste brand-logo center"><span class="brand-logo-pass">PASS</span> CENTER</a>
+    </nav>
+
+    <nav class="transparent">
+      <div class="nav-wrapper">
+          <div class="ajuste col s12">
+            <a href="#!" class="breadcrumb">First</a>
+            <a href="#!" class="breadcrumb">Second</a>
+            <a href="#!" class="breadcrumb">Third</a>
+          </div>
+        </div>
+    </nav>
+     
+
+  <ul id="slide-out" class="sidenav sidenav-fixed">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="./assets/02.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="./assets/01.jpg"></a>
+      <a href="#name"><span class="white-text name">Rafael A. B. Silva</span></a>
+      <a href="#email"><span class="white-text email">ID #9999999</span></a>
+    </div></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons">assignment</i>Minhas Disciplinas</a></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons">assignment_ind</i>Meus dados</a></li>
+    <li class="center-align sair"><a class="waves-effect waves-light btn red darken-4">Sair</a></li>
+  </ul>
+  <a href="#" data-target="slide-out" class="sidenav-trigger hide-on-med-and-up"><i class="material-icons">menu</i></a>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style scoped>
+    .sair{
+      bottom: 4rem;
+      left: 27%;
+      position: absolute;
+      display: inline;
+    }
+    
+    .brand-logo-pass{
+      color: black;
+      font-weight:900 !important;
+    }
+
+    .brand-logo{
+      color: black;
+      font-weight:100 !important;
+       
+    }
+    .breadcrumb::before{
+      color: black;
+    }
+    .breadcrumb{
+      color: black;
+    }
+    .ajuste {
+      padding-left: 300px;
+    }
+    .transparent{
+      padding-left: 20px;
+      box-shadow: none !important;
+    }
+
+    @media only screen and (max-width : 992px) {
+      .ajuste {
+        padding-left: 0;
+      }
+    }
+
+</style>
+
+<script src="./assets/meujs.js"></script>
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    ljk
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ola"
+  }
+</script>
+
+<style scoped>
+
+</style>
