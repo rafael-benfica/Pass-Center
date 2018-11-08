b126e9f4a30f413f791c894628f733e63e7aff00
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-11-08 19:47:28 -0200

    Atualizando Projeto - Parte 02

---
+++ Plataforma WEB/passcenter-project/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Professor from '@/components/professores/professor.vue'
+import MinhasDisciplinas from '@/components/professores/MinhasDisciplinas.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Professor,
+      children:[
+        {
+          path: '',
+          name: 'MinhasDisciplinas',
+          component: MinhasDisciplinas,
+          meta: {
+            breadcrumbs: [{ nome: 'Professor' },
+            { nome: 'Minhas Disciplinas' }]
+        }
+      }
+      ]
+    }
+  ]
+})
