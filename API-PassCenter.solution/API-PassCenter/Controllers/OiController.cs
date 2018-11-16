4b8559a3a7b5b44896b71ed6cbfee036e0da966f
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-11-16 10:43:57 -0200

    API start

---
+++ API-PassCenter.solution/API-PassCenter/Controllers/OiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_PassCenter.Controllers {
+    public class OiController : ApiController {
+        /// <summary>
+        /// olá tudo bem?
+        /// </summary>
+        /// <returns>oioioi</returns>
+        public IEnumerable<string> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
