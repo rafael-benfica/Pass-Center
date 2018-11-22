036709aa4ec40205292b6c6554169fa3603b78d4
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-11-22 16:28:59 -0200

    update icones

--- Plataforma WEB/passcenter-project/src/components/gerenteCadastro/DashBoardGerenteCadastro.vue
+++ Plataforma WEB/passcenter-project/src/components/gerenteCadastro/DashBoardGerenteCadastro.vue
@@ -26,9 +26,13 @@
       <a href="#name"><span class="white-text name">Nome do Usuário</span></a>
       <a href="#email"><span class="white-text email">ID #9999999</span></a>
     </div></li>
-    <li><a class=" waves-effect" href="#!"><i class="icone material-icons">assignment</i>Minhas Disciplinas</a></li>
-    <li><a class=" waves-effect" href="#!"><i class="icone material-icons">account_circle</i>Meus dados</a></li>
-        <li class="center-align botao-sair"><router-link :to="{ name: 'Login' }" class="waves-effect waves-light btn red darken-4">Sair</router-link></li>
+    <li><a class=" waves-effect" href="#!"><i class="icone material-icons">person</i>Alunos</a></li>
+    <li><a class=" waves-effect" href="#!"><i class="icone material-icons">supervisor_account</i>Professores</a></li>
+    <li><router-link :to="{ name: 'MeusDadosGerenteCadastro' }" class=" waves-effect"><i class="icone material-icons">account_circle</i>Meus Dados</router-link></li>
+    <li class="center-align botao-sair"><router-link :to="{ name: 'Login' }" class="waves-effect waves-light btn red darken-4">Sair</router-link></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Cursos</a></li>
+    <li v-for="item in disciplinas" :key="item.id"><a class="waves-effect" href="#!">{{ item.nome }}</a></li>
   </ul>
 
   <a href="#" data-target="slide-out" class="sidenav-trigger show-on-small "><i class="material-icons gatilho-mobile">menu</i></a>
