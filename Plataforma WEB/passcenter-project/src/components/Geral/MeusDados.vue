d624772397b52536d17dfbb754256d4a2e9ed554
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-11-22 16:48:51 -0200

    tornando responsivo

--- Plataforma WEB/passcenter-project/src/components/Geral/MeusDados.vue
+++ Plataforma WEB/passcenter-project/src/components/Geral/MeusDados.vue
@@ -5,25 +5,25 @@
                 <span class="card-title">Meus Dados</span>
             </div>
             <div class="row">
-                <div class="input-field col s6">
+                <div class="input-field col s12 m6">
                     <input id="nome" type="text" class="validate">
                     <label for="nome">Nome</label>
                 </div>
-                <div class="input-field col s6">
+                <div class="input-field col s12 m6">
                     <input id="sobrenome" type="text" class="validate">
                     <label for="sobrenome">Sobrenome</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <input id="cpf" type="text" class="validate">
                     <label for="cpf">CPF</label>
                 </div>
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <input id="rg" type="text" class="validate">
                     <label for="rg">RG</label>
                 </div>
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <select>
                         <option value="1">Masculino</option>
                         <option value="2">Feminino</option>
@@ -33,39 +33,39 @@
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <input id="rua" type="text" class="validate">
                     <label for="rua">Rua</label>
                 </div>
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <input id="numero" type="text" class="validate">
                     <label for="numero">Número</label>
                 </div>
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <input id="bairro" type="text" class="validate">
                     <label for="bairro">Bairro</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s4">
+                <div class="input-field col s12 m4">
                     <input id="cep" type="text" class="validate">
                     <label for="cep">CEP</label>
                 </div>
-                <div class="input-field col s6">
+                <div class="input-field col s12 m6">
                     <input id="municipio" type="text" class="validate">
                     <label for="municipio">Município</label>
                 </div>
-                <div class="input-field col s2">
+                <div class="input-field col s12 m2">
                     <input id="estado" type="text" class="validate">
                     <label for="estado">Estado</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s6">
+                <div class="input-field col s12 m6">
                     <input id="email" type="email" class="validate">
                     <label for="email">Email</label>
                 </div>
-                <div class="input-field col s6">
+                <div class="input-field col s12 m6">
                     <input id="password" type="password" class="validate">
                     <label for="password">Senha</label>
                 </div>
