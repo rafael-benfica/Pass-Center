805f81a37d9068b39720a0275cd47a396737ac51
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-11-30 16:10:06 -0200

    Update banco e classes

---
+++ API-PassCenter.solution/API-PassCenter/Models/Classes/Instituicoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_PassCenter.Models.Classes {
+    public class Instituicoes {
+
+        public int Ins_codigo { get; set; }
+        public string Ins_nome_fantasia { get; set; }
+        public string Ins_razao_social { get; set; }
+        public string Ins_inscricao_estadual { get; set; }
+        public string Ins_cnpj { get; set; }
+        public bool Ins_estado { get; set; }
+        public int Ins_periodo_renovacao_dias { get; set; }
+        public Endereco End_codigo { get; set; }
+
+    }
+}
---
+++ API-PassCenter.solution/API-PassCenter/Models/Classes/Pessoas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_PassCenter.Models.Classes {
+    public class Pessoas {
+        public int Pes_codigo { get; set; }
+        public string Pes_nome { get; set; }
+        public string Pes_sobrenomes { get; set; }
+        public int Pes_sexo { get; set; }
+        public string Pes_matricula { get; set; }
+        public string Pes_cpf { get; set; }
+        public string Pes_tel_residencial { get; set; }
+        public string Pes_tel_celular { get; set; }
+        public string Pes_info_adicionais { get; set; }
+        public Endereco End_codigo { get; set; }
+        public Instituicoes Ins_codigo { get; set; }
+    }
+}
