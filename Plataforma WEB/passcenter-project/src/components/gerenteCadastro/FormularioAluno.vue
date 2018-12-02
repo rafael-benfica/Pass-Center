aca1ea730d96a3774d6d92cd975f4d1fb1b1c4b4
JoaoPauloCunha <jcpfcunha@gmail.com>
2018-12-01 22:03:48 -0200

    Correção de Pontuação

--- Plataforma WEB/passcenter-project/src/components/gerenteCadastro/FormularioAluno.vue
+++ Plataforma WEB/passcenter-project/src/components/gerenteCadastro/FormularioAluno.vue
@@ -103,7 +103,7 @@
 								
         							<div class="input-field col s12 m12 l12">
           								<textarea id="textarea1" class="materialize-textarea"></textarea>
-          									<label for="textarea1">Informações Adicionais</label>
+          									<label for="textarea1">Informações Adicionais:</label>
         							</div>
       							
 
