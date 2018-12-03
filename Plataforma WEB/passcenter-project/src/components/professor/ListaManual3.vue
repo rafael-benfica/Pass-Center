e6d0a4474315eb389394253e74cf096b5eeec143
JoaoPauloCunha <jcpfcunha@gmail.com>
2018-12-03 19:31:15 -0200

    update

--- Plataforma WEB/passcenter-project/src/components/professor/ListaManual3.vue
+++ Plataforma WEB/passcenter-project/src/components/professor/ListaManual3.vue
@@ -10,36 +10,45 @@
                                 <td>Nome do Aluno</td>
                                 <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
                                 <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
                                 <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
                                 <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
                                 <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
                                 <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
                                 <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
                                 <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
                                 <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
                                 <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
                                 <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
                                 <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
-                            
+                                                    
                         </tbody>
                     </table>
-                        
+                        <div class="col s12 m12 l12 historico data">
+                            <button class="btn waves-effect waves-light" type="submit" name="action">Salvar</button>                        
+                             <button class="btn waves-effect waves-light" type="submit" name="action">Cancelar</button>
+                        </div>
             </div> 
             </div>
         </form>
