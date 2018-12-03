f01a9faa764422852041351f062c4c7a4dbddecd
JoaoPauloCunha <jcpfcunha@gmail.com>
2018-12-03 20:02:27 -0200

    update

--- Plataforma WEB/passcenter-project/src/components/professor/ListaManual3.vue
+++ Plataforma WEB/passcenter-project/src/components/professor/ListaManual3.vue
@@ -8,54 +8,89 @@
                         <tbody>
                              <tr>
                                 <td>Nome do Aluno</td>
-                                <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
-                                <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="btn botao green"><i class="material-icons">check</i></a></td>
+                                <td><a class="btn botao red"><i class="material-icons">close</i></a></td>
                                 <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
-                                <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
-                                <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="btn botao green"><i class="material-icons">check</i></a></td>
+                                <td><a class="btn botao red"><i class="material-icons">close</i></a></td>
                                 <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
-                                <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
-                                <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="btn botao green"><i class="material-icons">check</i></a></td>
+                                <td><a class="btn botao red"><i class="material-icons">close</i></a></td>
                                 <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
-                                <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
-                                <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="btn botao green"><i class="material-icons">check</i></a></td>
+                                <td><a class="btn botao red"><i class="material-icons">close</i></a></td>
                                 <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
-                                <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
-                                <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="btn botao green"><i class="material-icons">check</i></a></td>
+                                <td><a class="btn botao red"><i class="material-icons">close</i></a></td>
                                 <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
                             <tr>
                                 <td>Nome do Aluno</td>
-                                <td><a class="btn modal-trigger botao green" href="#modal1"><i class="material-icons">check</i></a></td>
-                                <td><a class="btn modal-trigger botao red" href="#modal1"><i class="material-icons">close</i></a></td>
+                                <td><a class="btn botao green"><i class="material-icons">check</i></a></td>
+                                <td><a class="btn botao red"><i class="material-icons">close</i></a></td>
                                 <td><a class="botaofe waves-effect btn-flat">Desfazer</a></td>
                             </tr>
+                            
+                            
                                                     
                         </tbody>
                     </table>
                         <div class="col s12 m12 l12 historico data">
-                            <button class="btn waves-effect waves-light" type="submit" name="action">Salvar</button>                        
+                            <button class="waves-effect waves-light btn modal-trigger botaoSalvar" href="#modal1" type="submit" name="action">Salvar</button>               
+                         <router-link :to="{ name: 'ListaManual2' }" class=" waves-effect">             
                              <button class="btn waves-effect waves-light" type="submit" name="action">Cancelar</button>
+                         </router-link>
+	
                         </div>
-            </div> 
+                        <div id="modal1" class="modal">
+			                 <div class="modal-content">
+                                  <h2 class="modalex">Você tem Certeza?</h2>
+                             </div>
+                    
+		                    <div class="modal-footer row col s12 m12 l12 ">
+                                    
+                                <div class="modalex">
+                                    <a href="#!" class="col s12 m3 l3 modal-close waves-effect waves-teal btn red">Cancelar</a>
+					
+					            	<p class="col s12 m3 l3"></p>
+					
+					            	<a href="#!" class="col s12 m3 l3 modal-close waves-effect waves-teal btn green">Confirmar</a>
+                                </div>
+                                    
+		    	            </div>
+                         </div>
+            
+	            </div>
             </div>
         </form>
     </div>
 </template>
 
+<script>
+	export default {
+		name: "ListaManual3",
 
-<script src="./../../assets/js/professor/ListaManual.js"></script>
+		mounted: function () {
+			$(document).ready(function () {
+				$('.modal').modal();
+			});
+		},
+		
+	}
+
+</script>
+
 <style src="./../../assets/css/professor/MinhasDisciplinas.css" scoped></style>
    
