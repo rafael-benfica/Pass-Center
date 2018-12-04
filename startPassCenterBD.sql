89c364b5e9ac63fb48d2f1f71574a3e3d634269a
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2018-12-04 18:18:04 -0200

    update

--- startPassCenterBD.sql
+++ startPassCenterBD.sql
@@ -1,28 +1,77 @@
 use passcenter;
 
+-- Inserindo Tipos de Usuários
 INSERT INTO tipos_usuarios (`tus_titulo`) VALUES ('Administrador');
-
 INSERT INTO tipos_usuarios (`tus_titulo`) VALUES ('Gerente Geral');
-
 INSERT INTO tipos_usuarios (`tus_titulo`) VALUES ('Gerente Cadastro');
+INSERT INTO tipos_usuarios (`tus_titulo`) VALUES ('Auditor');
+INSERT INTO tipos_usuarios (`tus_titulo`) VALUES ('Aluno');
 
-INSERT INTO tipos_usuarios (`tus_titulo`) VALUES ('aluno');
 
-INSERT INTO tipos_enderecos (`ten_titulo`) VALUES ('instituição');
+-- Inserindo Tipos de Endereço
+INSERT INTO tipos_enderecos (`ten_titulo`) VALUES ('Instituição');
+INSERT INTO tipos_enderecos (`ten_titulo`) VALUES ('Pessoa');
 
-INSERT INTO tipos_enderecos (`ten_titulo`) VALUES ('pessoa');
 
+-- Inserindo Instituição
 INSERT INTO enderecos(end_logradouro, end_numero, end_bairro, end_municipio, end_cep, end_estado, end_complemento, end_pais, ten_codigo)
-VALUES('rua', 'numero', 'bairro', 'municipio', '00000000', '1', 'complemento', 'pais', '1');
-
+VALUES('Av. Prof. João Rodrigues', '1501', 'Jardim Esperança', 'Guaratinguetá', '12518150', 'SP', 'complemento', 'Brasil', '1');
 INSERT INTO instituicoes(ins_nome_fantasia, ins_razao_social, ins_inscricao_estadual, ins_cnpj, ins_estado, ins_periodo_renovacao_dias, end_codigo)
-VALUES('Nome Fantasia', 'Razão Social', 'Inscrição Estadual', '00.000.000/0000-00', '1', '30', '1');
+VALUES('FATEC Guaratinguetá', 'Faculdade de Tecnologia Estadual de Guaratinguetá', '000.000.000.000', '00.000.000/0000-00', '1', '30', '1');
 
+
+-- Inserindo Usuários
+
+-- Inserindo Administrador
 INSERT INTO enderecos(end_logradouro, end_numero, end_bairro, end_municipio, end_cep, end_estado, end_complemento, end_pais, ten_codigo)
-VALUES('rua', 'numero', 'bairro', 'municipio', '00000000', '1', 'complemento', 'pais', '2');
+VALUES('Rua Teerã', '930', 'Parque da Lapa', 'São Paulo', '05301000', '1', '', 'Brasil', '2');
 
 INSERT INTO pessoas (pes_nome, pes_sobrenomes, pes_cpf, pes_rg, pes_matricula, pes_sexo, pes_tel_residencial, pes_tel_celular, pes_info_adicionais, end_codigo, ins_codigo) 
-VALUES ('João', 'Silva', '000.000.000-00', '00.000.000-0', '0000000', '1', '0000-0000', '90000-0000', 'Informações Adicionais','2', '1');
+VALUES ('Filipe', 'Márcio Ferreira', '240.436.648-34', '48.085.566-3', '1802010', '1', '(11) 2511-5724', '(11) 98403-6407', '','2', '1');
 
 INSERT INTO usuarios (usu_login, usu_senha, usu_estado, usu_data_criacao, usu_data_desativacao, usu_primeiro_login, usu_redefinir_senha, pes_codigo, tus_codigo) 
-VALUES ('joao', 'oi', '1', '2018-01-12 00:00:00', '0001-01-01 00:00:00', '0', '0', '1', '1');
+VALUES ('felipe', 'oi', '1', '2018-01-12 00:00:00', '0001-01-01 00:00:00', '0', '0', '1', '1');
+
+
+-- Inserindo Gerente Geral
+INSERT INTO enderecos(end_logradouro, end_numero, end_bairro, end_municipio, end_cep, end_estado, end_complemento, end_pais, ten_codigo)
+VALUES('Rua Presidente João Café Filho', '592', 'Jardim Ipanema', 'Santo André', '09121680', 'SP', '', 'Brasil', '2');
+
+INSERT INTO pessoas (pes_nome, pes_sobrenomes, pes_cpf, pes_rg, pes_matricula, pes_sexo, pes_tel_residencial, pes_tel_celular, pes_info_adicionais, end_codigo, ins_codigo) 
+VALUES ('Nicole', 'Bruna Aline Freitas', '309.127.538-89', '33.609.202-7', '1802020', '2', '(11) 2706-6469', '(11) 98193-4069', '','3', '1');
+
+INSERT INTO usuarios (usu_login, usu_senha, usu_estado, usu_data_criacao, usu_data_desativacao, usu_primeiro_login, usu_redefinir_senha, pes_codigo, tus_codigo) 
+VALUES ('nicole', 'oi', '1', '2018-01-12 00:00:00', '0001-01-01 00:00:00', '0', '0', '2', '2');
+
+
+-- Inserindo Gerente Cadastro
+INSERT INTO enderecos(end_logradouro, end_numero, end_bairro, end_municipio, end_cep, end_estado, end_complemento, end_pais, ten_codigo)
+VALUES('Rua Hugo Prado', '954', 'Jardim Amaralina', 'São Paulo', '05570210', 'SP', '', 'Brasil', '2');
+
+INSERT INTO pessoas (pes_nome, pes_sobrenomes, pes_cpf, pes_rg, pes_matricula, pes_sexo, pes_tel_residencial, pes_tel_celular, pes_info_adicionais, end_codigo, ins_codigo) 
+VALUES ('Yuri', 'Felipe Castro', '536.525.748-00', '14.648.632-8', '1802030', '1', '(11) 2545-4040', '(11) 99272-4760', '','4', '1');
+
+INSERT INTO usuarios (usu_login, usu_senha, usu_estado, usu_data_criacao, usu_data_desativacao, usu_primeiro_login, usu_redefinir_senha, pes_codigo, tus_codigo) 
+VALUES ('yuri', 'oi', '1', '2018-01-12 00:00:00', '0001-01-01 00:00:00', '0', '0', '3', '3');
+
+
+-- Inserindo Auditor
+INSERT INTO enderecos(end_logradouro, end_numero, end_bairro, end_municipio, end_cep, end_estado, end_complemento, end_pais, ten_codigo)
+VALUES('Rua Fábio Tullio de Mattos', '481', 'Jardim Maria Amélia', 'Jacareí', '12318280', 'SP', '', 'Brasil', '2');
+
+INSERT INTO pessoas (pes_nome, pes_sobrenomes, pes_cpf, pes_rg, pes_matricula, pes_sexo, pes_tel_residencial, pes_tel_celular, pes_info_adicionais, end_codigo, ins_codigo) 
+VALUES ('Bruna', 'Benedita Alessandra Drumond', '730.056.198-50', '26.499.183-7', '1802040', '2', '(12) 2630-2701', '(12) 99159-8474', '','5', '1');
+
+INSERT INTO usuarios (usu_login, usu_senha, usu_estado, usu_data_criacao, usu_data_desativacao, usu_primeiro_login, usu_redefinir_senha, pes_codigo, tus_codigo) 
+VALUES ('bruna', 'oi', '1', '2018-01-12 00:00:00', '0001-01-01 00:00:00', '0', '0', '4', '4');
+
+
+-- Inserindo Aluno
+INSERT INTO enderecos(end_logradouro, end_numero, end_bairro, end_municipio, end_cep, end_estado, end_complemento, end_pais, ten_codigo)
+VALUES('Rua Soldado Amarilho Gonçalves Queiroz', '302', 'Ponte Grande', 'Guarulhos', '07032200', 'SP', '', 'Brasil', '2');
+
+INSERT INTO pessoas (pes_nome, pes_sobrenomes, pes_cpf, pes_rg, pes_matricula, pes_sexo, pes_tel_residencial, pes_tel_celular, pes_info_adicionais, end_codigo, ins_codigo) 
+VALUES ('Diego', 'Daniel Cardoso', '313.172.158-85', '15.498.605-7', '1802050', '1', '(11) 2840-1247', '(11) 99614-1502', '','6', '1');
+
+INSERT INTO usuarios (usu_login, usu_senha, usu_estado, usu_data_criacao, usu_data_desativacao, usu_primeiro_login, usu_redefinir_senha, pes_codigo, tus_codigo) 
+VALUES ('Diego', 'oi', '1', '2018-01-12 00:00:00', '0001-01-01 00:00:00', '0', '0', '5', '5');
