6f5fbdaf99362397fb05cb5e69fd3ad48ee39c9a
JoaoPauloCunha <jcpfcunha@gmail.com>
2018-12-04 02:24:08 -0200

    Update

---
+++ Plataforma WEB/passcenter-project/src/components/gerenteGeral/DisciplinasGeral.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="row area-exibicao">
+            <div class="col s12 m4 l4 espacamento" v-for="item in 6" :key="item.id">
+                <div class="card">
+                    <div class="card-content">
+                        <h2 class="card-title cardDisciplina">Engenharia de Softwares {{ item }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+
+
+<style src="./../../assets/css/aluno/MinhasDisciplinasAluno.css" scoped></style>
