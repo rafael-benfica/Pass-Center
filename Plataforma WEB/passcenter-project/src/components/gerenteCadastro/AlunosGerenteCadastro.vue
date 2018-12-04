396a59a696f9c87c635ed199170b67511dadcaf4
JoaoPauloCunha <jcpfcunha@gmail.com>
2018-12-04 02:44:27 -0200

    LastCommitofTheDay

--- Plataforma WEB/passcenter-project/src/components/gerenteCadastro/AlunosGerenteCadastro.vue
+++ Plataforma WEB/passcenter-project/src/components/gerenteCadastro/AlunosGerenteCadastro.vue
@@ -99,9 +99,37 @@
                                     </div>
 
                                     <div class="input-field col s12 m3 l3">
-                                            <input id="estado" type="text" class="validate">
-                                            <label for="estado">Estado:</label>
-                                    </div>
+                    <select v-model="estado">
+                        <option value="AC">Acre</option>
+                        <option value="AL">Alagoas</option>
+                        <option value="AP">Amapá</option>
+                        <option value="AM">Amazonas</option>
+                        <option value="BA">Bahia</option>
+                        <option value="CE">Ceará</option>
+                        <option value="DF">Distrito Federal</option>
+                        <option value="ES">Espírito Santo</option>
+                        <option value="GO">Goiás</option>
+                        <option value="MA">Maranhão</option>
+                        <option value="MT">Mato Grosso</option>
+                        <option value="MS">Mato Grosso do Sul</option>
+                        <option value="MG">Minas Gerais</option>
+                        <option value="PA">Pará</option>
+                        <option value="PB">Paraíba</option>
+                        <option value="PR">Paraná</option>
+                        <option value="PE">Pernambuco</option>
+                        <option value="PI">Piauí</option>
+                        <option value="RJ">Rio de Janeiro</option>
+                        <option value="RN">Rio Grande do Norte</option>
+                        <option value="RS">Rio Grande do Sul</option>
+                        <option value="RO">Rondônia</option>
+                        <option value="RR">Roraima</option>
+                        <option value="SC">Santa Catarina</option>
+                        <option value="SP">São Paulo</option>
+                        <option value="SE">Sergipe</option>
+                        <option value="TO">Tocantins</option>
+                    </select>
+                    <label>Estado:</label>
+                </div>
 
                                     <div class="input-field col s12 m3 l3">
                                             <input id="cidade" type="text" class="validate">
