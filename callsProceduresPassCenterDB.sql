56fff9d02fbcd3f9566972d2eb50a284f49b3e6d
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2019-04-21 17:30:25 -0300

    Ajustes nos scripts de geração do Banco de Dados

---
+++ callsProceduresPassCenterDB.sql
@@ -0,0 +1,5 @@
+-- Precedure InserirPresenca
+call InserirPresenca(1, 4,'(0)', '00:00:01', '00:00:01', 1);
+
+-- Precedure MigrarAno
+call MigrarAno(1,"2019.1");
--- proceduresPassCenterDB.sql
+++ proceduresPassCenterDB.sql
@@ -1,4 +1,7 @@
-drop procedure InserirPresenca;
+
+
+-- Precedure InserirPresenca
+drop procedure if exists InserirPresenca;
 
 DELIMITER $
 CREATE PROCEDURE `InserirPresenca`(in vEau_codigo int, in vPes_codigo int, in list_of_ids text, in vPre_horario_entrada datetime, in vPre_horario_saida datetime, in vSes_codigo int)  
@@ -50,4 +53,45 @@ CREATE PROCEDURE `InserirPresenca`(in vEau_codigo int, in vPes_codigo int, in li
 	END;
 $
 
-call InserirPresenca(1, 4,'(0)', '00:00:01', '00:00:01', 1);
+
+-- Precedure MigrarAno
+drop procedure if exists MigrarAno;
+
+delimiter $
+CREATE PROCEDURE MigrarAno(in vInstituicao int, in vPeriodo varchar(45))
+begin
+	declare _eau_codigo INT;
+	declare _eau_periodo_identificacao VARCHAR(45);
+	declare _eau_estado TINYINT;
+	declare _eau_data_abertura DATETIME;
+	declare _eau_data_fechamento DATETIME;
+	declare _pes_codigo INT;
+	declare _eve_codigo INT;
+	declare _ins_codigo INT;
+
+	declare i INT;
+	declare total INT;
+
+	declare c1 cursor for select * from enventos_auditores where ins_codigo = vInstituicao and eau_estado = 1;
+
+	set i = 0;
+	set total = 0;
+
+	select count(*) into total from enventos_auditores where ins_codigo = vInstituicao and eau_estado = 1;
+
+
+	open c1;
+		while i<total do
+			fetch c1 into _eau_codigo, _eau_periodo_identificacao, _eau_estado, _eau_data_abertura, 
+			_eau_data_fechamento, _pes_codigo, _eve_codigo, _ins_codigo;
+			
+			update enventos_auditores set eau_estado = 0, eau_data_fechamento = NOW() where eau_codigo = _eau_codigo;
+			
+			insert into enventos_auditores values (0, vPeriodo, 1, NOW(), null, _pes_codigo, _eve_codigo, _ins_codigo);
+			
+		   set i = i + 1;
+		
+		end while;
+	close c1;
+end
+$
