38e2bb12804a38d66a11a56761a6c0230ffc9a5c
Rafael Alcântara <ra.fa.alcantara@hotmail.com>
2019-07-02 17:04:16 -0300

    update

--- callsProceduresPassCenterDB.sql
+++ callsProceduresPassCenterDB.sql
@@ -5,4 +5,4 @@ call InserirPresenca(1, 4,'6', '00:00:01', '00:00:01', 1);
 call InserirPresenca(1, 4,'5,6', '00:00:01', '00:00:01', 1);
 
 -- Precedure MigrarAno
-call MigrarAno(1,"2019.1");
+call MigrarAno2(2,"2021");
